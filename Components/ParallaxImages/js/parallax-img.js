2ddb73e62a97824b4ca01f19b71559b52a3c4939
kkashy <kshemender.kashyap@truinc.ca>
2022-12-02 14:45:36 +0530

    lenght added

--- Components/ParallaxImages/js/parallax-img.js
+++ Components/ParallaxImages/js/parallax-img.js
@@ -1,7 +1,9 @@
-jQuery('.convertedImage').each(function() {
-    var width = jQuery(window).width();
-    if(width > 767){
-        var img_src = jQuery(this).find('.desktop-image').attr('src');       
-        jQuery(this).css('background-image','url("'+ img_src +'")');
-    }  
-});
+if (jQuery('.convertedImage').length > 0){
+    jQuery('.convertedImage').each(function() {
+        var width = jQuery(window).width();
+        if(width > 767){
+            var img_src = jQuery(this).find('.desktop-image').attr('src');       
+            jQuery(this).css('background-image','url("'+ img_src +'")');
+        }  
+    });
+}
