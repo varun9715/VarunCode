878ed464fd4a46bfafe73d7d6456fbbbee8a3e8d
Shruti Verma <shruti@tru.agency>
2022-12-13 17:04:51 +0530

    parallax animation added

--- Components/ParallaxImages/js/parallax.js
+++ Components/ParallaxImages/js/parallax.js
@@ -1,1 +1,20 @@
-new WOW().init(); 
+new WOW().init();
+jQuery(document).ready(function($){
+     'use strict';
+  
+    $.Scrollax();
+  });
+
+  var scrollActive=document.querySelectorAll('.infoPanel');
+
+  window.addEventListener('scroll',function(){
+    for(let i=0;i<scrollActive.length;i++){
+      var data=parseFloat(scrollActive[i]?.style?.transform?.split('translateY')[1]?.split(/[()]/)[1])
+      if(data>0){
+        scrollActive[i].classList.add("infoPanelactive")
+      }
+      else{
+        scrollActive[i].classList.remove("infoPanelactive")
+      }
+    }
+  })
